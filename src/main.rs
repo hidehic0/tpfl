cc79602e33552b843dada10cae77631516970360
hidehic0 <hidehico.ooba@gmail.com>
2025-12-30 18:32:04 +0900

    feat: add a feature to download or copy file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ mod load_config;
 mod types;
 
 use clap::Parser;
-use std::path::PathBuf;
+use std::{collections::HashMap, fs, path::Path, path::PathBuf, process};
+use walkdir::WalkDir;
+use xdg;
 
 #[derive(Parser)]
 struct Cli {
@@ -14,4 +16,102 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
+
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("tpfl");
+    let config_path = match xdg_dirs.get_config_home() {
+        Some(p) => p,
+        None => {
+            eprintln!("failed to get config dir");
+            process::exit(1);
+        }
+    };
+
+    if !config_path.is_dir() {
+        match fs::create_dir_all(&config_path) {
+            Ok(_) => {}
+            Err(_) => {
+                eprintln!("couldn't create config dir");
+                process::exit(1)
+            }
+        }
+    }
+
+    let mut configs: HashMap<String, types::Template> = HashMap::new();
+
+    for entry in WalkDir::new(&config_path).follow_links(true) {
+        let path = entry.unwrap().into_path();
+
+        if path.extension() != Some("yaml".as_ref()) {
+            continue;
+        }
+
+        let templates = match load_config::load_config(path) {
+            Ok(t) => t,
+            Err(e) => {
+                eprintln!("{}", e);
+                process::exit(1);
+            }
+        };
+
+        for template in templates.templates {
+            configs.insert(template.name.clone(), template);
+        }
+    }
+
+    if configs.get(&cli.name) == None {
+        eprintln!("couldn't find {} in the config", &cli.name);
+        process::exit(1);
+    }
+
+    let template = configs.get(&cli.name).unwrap();
+    let mut new_path = Path::new(".").join(match cli.output_path {
+        Some(p) => p.to_str().unwrap().to_string(),
+        None => template.file_name.clone(),
+    });
+
+    if template.file_type == "path" {
+        match fs::copy(template.path.clone(), new_path) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("{}", e);
+                process::exit(1);
+            }
+        };
+    } else if template.file_type == "url" {
+        let mut response = match reqwest::blocking::get(template.path.clone()) {
+            Ok(r) => r,
+            Err(e) => {
+                eprintln!("{}", e);
+                process::exit(1);
+            }
+        };
+
+        response = match response.error_for_status() {
+            Ok(r) => r,
+            Err(e) => {
+                eprintln!("{}", e);
+                process::exit(1);
+            }
+        };
+
+        let mut out = match fs::OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .open(new_path)
+        {
+            Ok(o) => o,
+            Err(e) => {
+                eprintln!("{}", e);
+                process::exit(1);
+            }
+        };
+
+        match std::io::copy(&mut response, &mut out) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("{}", e);
+                process::exit(1);
+            }
+        };
+    }
 }
--- src/types.rs
+++ src/types.rs
@@ -5,11 +5,11 @@ pub struct Config {
     pub templates: Vec<Template>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, PartialEq)]
 pub struct Template {
     pub name: String,
     #[serde(rename(deserialize = "filename"))]
-    pub file_name: Option<String>,
+    pub file_name: String,
     #[serde(rename(deserialize = "type"))]
     pub file_type: String,
     pub path: String,
